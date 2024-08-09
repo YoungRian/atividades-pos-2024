a020266b67e6313afd577d795005d87eb91b14d8
Joedson <joedson.rian@escolar.ifrn.edu.br>
2024-08-09 14:17:35 -0300

    Criei o codigo para acessar as informações de cada cardapio

--- Parcers/cardapio.py
+++ Parcers/cardapio.py
@@ -1,3 +1,36 @@
 from xml.dom.minidom import parse
 
-dom = parse("")
+dom = parse("Parcers/cardapio.xml")
+
+# Elemento raiz do XML (cardapio.xml)
+cardapio = dom.documentElement
+
+# Recebe uma lista dos elementos com tag "prato"
+pratos = cardapio.getElementsByTagName('prato')
+
+# Acessa as informações de cada prato
+for prato in pratos:
+    idprato = prato.getAttribute('id')
+
+    elemento_nome = prato.getElementsByTagName('nome')[0]
+    nome = elemento_nome.firstChild.nodeValue
+    
+    elemento_descricao = prato.getElementsByTagName('descricao')[0]
+    descricao = elemento_descricao.firstChild.nodeValue
+
+    elemento_preco = prato.getElementsByTagName('preco')[0]
+    preco = elemento_preco.firstChild.nodeValue
+
+    elemento_calorias = prato.getElementsByTagName('calorias')[0]
+    calorias = elemento_calorias.firstChild.nodeValue
+
+    elemento_tempo = prato.getElementsByTagName('tempo')[0]
+    tempo = elemento_tempo.firstChild.nodeValue
+
+    print("Prato :", idprato)
+    print("Nome:", nome)
+    print("Descrição:", descricao)
+    print("Preço: ", preco)
+    print("Calorias: ", calorias)
+    print("Tempo: ", tempo)
+    print("-----------\n")
