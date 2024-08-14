95f92d9fa54a1faed8fcb5f7a6e6643ac9028713
Joedson <joedson.rian@escolar.ifrn.edu.br>
2024-08-14 17:19:23 -0300

    Adicionei linhas em python para buscar pratos do arquivo cardapio.xml

--- Parcers/cardapio.py
+++ Parcers/cardapio.py
@@ -8,29 +8,72 @@ cardapio = dom.documentElement
 # Recebe uma lista dos elementos com tag "prato"
 pratos = cardapio.getElementsByTagName('prato')
 
+# Mostrar pratos
+for List_Prato in pratos:
+    List_Prato_ID = List_Prato.getAttribute('id')
+
+    elemento_list_nome = List_Prato.getElementsByTagName('nome')[0]
+    list_nome = elemento_list_nome.firstChild.nodeValue
+    
+    elemento_list_descricao = List_Prato.getElementsByTagName('descricao')[0]
+    list_descricao = elemento_list_descricao.firstChild.nodeValue
+        
+    elemento_list_preco = List_Prato.getElementsByTagName('preco')[0]
+    list_preco = elemento_list_preco.firstChild.nodeValue
+
+    elemento_list_calorias = List_Prato.getElementsByTagName('calorias')[0]
+    list_calorias = elemento_list_calorias.firstChild.nodeValue
+
+    elemento_list_tempo = List_Prato.getElementsByTagName('tempo')[0]
+    list_tempo = elemento_list_tempo.firstChild.nodeValue
+
+    print("Prato ID: ", List_Prato_ID)
+    print("Nome:", list_nome)
+    print("Descrição:", list_descricao)
+    print("Preço: ", list_preco)
+    print("Calorias: ", list_calorias)
+    print("Tempo: ", list_tempo)
+    print("-----------\n")
+    
+# Para o usuario digitar o ID do prato
+pratoID = input("Digite o ID do prato para mais informações: ")
+
+# Para iniciar uma flag e verificar as informações do prato
+prato_encontrado = False
+
 # Acessa as informações de cada prato
 for prato in pratos:
     idprato = prato.getAttribute('id')
 
-    elemento_nome = prato.getElementsByTagName('nome')[0]
-    nome = elemento_nome.firstChild.nodeValue
-    
-    elemento_descricao = prato.getElementsByTagName('descricao')[0]
-    descricao = elemento_descricao.firstChild.nodeValue
+    if idprato == pratoID:
 
-    elemento_preco = prato.getElementsByTagName('preco')[0]
-    preco = elemento_preco.firstChild.nodeValue
+        elemento_nome = prato.getElementsByTagName('nome')[0]
+        nome = elemento_nome.firstChild.nodeValue
+        
+        elemento_descricao = prato.getElementsByTagName('descricao')[0]
+        descricao = elemento_descricao.firstChild.nodeValue
 
-    elemento_calorias = prato.getElementsByTagName('calorias')[0]
-    calorias = elemento_calorias.firstChild.nodeValue
+        elemento_preco = prato.getElementsByTagName('preco')[0]
+        preco = elemento_preco.firstChild.nodeValue
 
-    elemento_tempo = prato.getElementsByTagName('tempo')[0]
-    tempo = elemento_tempo.firstChild.nodeValue
+        elemento_calorias = prato.getElementsByTagName('calorias')[0]
+        calorias = elemento_calorias.firstChild.nodeValue
 
-    print("Prato :", idprato)
-    print("Nome:", nome)
-    print("Descrição:", descricao)
-    print("Preço: ", preco)
-    print("Calorias: ", calorias)
-    print("Tempo: ", tempo)
-    print("-----------\n")
+        elemento_tempo = prato.getElementsByTagName('tempo')[0]
+        tempo = elemento_tempo.firstChild.nodeValue
+
+        print("Prato ID: ", idprato)
+        print("Nome:", nome)
+        print("Descrição:", descricao)
+        print("Preço: ", preco)
+        print("Calorias: ", calorias)
+        print("Tempo: ", tempo)
+        print("-----------\n")
+
+        prato_encontrado = True
+
+        break
+
+# Se a flag "prato_encontrado" ainda for igual a falso
+if not prato_encontrado: # ou if prato_encontrado == False
+    print("ID do prato não encontrado.")
